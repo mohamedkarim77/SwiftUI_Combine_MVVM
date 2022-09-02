35ec209ccc051beb3a8b723dc3c33fa5d366a4b0
Mohamed Abdelkarim <d.mhmdkarim@gmail.com>
2022-09-02 11:26:01 +0400

    Initial Commit

---
+++ SwiftUI_Combine_MVVM/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI_Combine_MVVM
+//
+//  Created by Mohamed Abdelkarim on 02/09/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI_Combine_MVVM/SwiftUI_Combine_MVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Combine_MVVMApp.swift
+//  SwiftUI_Combine_MVVM
+//
+//  Created by Mohamed Abdelkarim on 02/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Combine_MVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
